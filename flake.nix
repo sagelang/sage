680e7d9a2c8436b06b5a03c0f9ca4b4cda9141fc
neriumpete <pavlovskipetko@protonmail.com>
2026-03-13 13:54:10 +0200

    feat: add Nix flake

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Sage - A programming language where agents are first-class citizens";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
+        sage = rustPlatform.buildRustPackage {
+          pname = "sage";
+          version = "0.1.1";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          # Build only the CLI
+          cargoBuildFlags = [ "-p" "sage-cli" ];
+
+          meta = with pkgs.lib; {
+            description = "A programming language where agents are first-class citizens";
+            homepage = "https://github.com/cargopete/sage";
+            license = licenses.mit;
+            maintainers = [ ];
+          };
+        };
+      in
+      {
+        packages = {
+          default = sage;
+          sage = sage;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = sage;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+        };
+      }
+    );
+}
